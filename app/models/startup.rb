1894c42baca13cbd199daca9a5d363c9928be8e6
vavax0 <vavax0@gmail.com>
2014-08-07 10:22:49 +0200

    Changed thumbnail BIG style size

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -3,7 +3,7 @@ class Startup < ActiveRecord::Base
 	belongs_to :category
 
 	has_attached_file :website_thumbnail, :storage => :dropbox, 
-					  :styles => { :big => "600x450", :medium => "400x300", :thumb => "250x200" },
+					  :styles => { :big => "800x500", :medium => "400x300", :thumb => "250x200" },
 					  :dropbox_credentials => "#{Rails.root}/config/dropbox.yml", 
 					  :dropbox_options => { :path => proc { |style| "#{style}/#{id}_#{website_thumbnail.original_filename}.jpg" } }
 										
