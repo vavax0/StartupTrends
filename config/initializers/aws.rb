4188fa4274c0593cf419d6510cf925ee7d7aa49b
vavax0 <vavax0@gmail.com>
2014-08-17 22:41:52 +0200

    Deleted files from github

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 AWS.config({
-    :access_key_id => 'AKIAJJJF53XQYIJ5RU2Q',
-    :secret_access_key => '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',
+    :access_key_id => '',
+    :secret_access_key => ''
   })
