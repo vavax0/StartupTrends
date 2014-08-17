39d71dcd9f3b981457a8531660606164470da11f
vavax0 <vavax0@gmail.com>
2014-08-17 23:05:04 +0200

    fixed untracked files

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-	:address				=> "smtp.gmail.com",
-	:port					=> 587,
-	:domain					=> "gmail.com",
-	:user_name				=> "vavax0@gmail.com",
-	:password				=> "pawel123",
-	:authentication			=> "plain",
-	:enable_starttls_auto	=> true
-}
