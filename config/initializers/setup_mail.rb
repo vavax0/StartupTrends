3e42f4e106c6687ed29aeff031051827c7f82333
vavax0 <vavax0@gmail.com>
2014-08-18 13:53:40 +0200

    Added VAriables

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-	:address				=> ENV["EMAIL_ADDRESS"]
-	:port					=> ENV["EMAIL_PORT"]
-	:domain					=> ENV["EMAIL_DOMAIN"]
-	:user_name				=> ENV["EMAIL_USER_NAME"]
-	:password				=> ENV["EMAIL_PASSWORD"]
-	:authentication			=> ENV["EMAIL_AUTHENTICATION"]
+	:address				=> ENV["EMAIL_ADDRESS"],
+	:port					=> ENV["EMAIL_PORT"],
+	:domain					=> ENV["EMAIL_DOMAIN"],
+	:user_name				=> ENV["EMAIL_USER_NAME"],
+	:password				=> ENV["EMAIL_PASSWORD"],
+	:authentication			=> ENV["EMAIL_AUTHENTICATION"],
 	:enable_starttls_auto	=> ENV["EMAIL_ENABLE_STARTTLS_AUTO"]
 }
