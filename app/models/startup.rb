21c5f7e806081bb035f8212e43da883edb85a9f4
vavax0 <vavax0@gmail.com>
2014-08-27 12:06:46 +0200

    Updated tags method

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -53,11 +53,11 @@ class Startup < ActiveRecord::Base
 	def tag_list
 		self.tags.collect do |tag|
 			tag.name
-		end.join(", ")
+		end.join(",")
 	end
 
 	def tag_list=(tag_string)
-		tag_names = tag_string.split(", ").collect{|s| s.strip.downcase }.uniq
+		tag_names = tag_string.split(",").collect{|s| s.strip.downcase }.uniq
 		new_or_found_tags = tag_names.collect{ |name| Tag.find_or_create_by(name: name) }
 		self.tags = new_or_found_tags
 	end
